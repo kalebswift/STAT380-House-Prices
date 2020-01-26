3c1705a53bc782d9346d215862a6efd55c7a8168
kalebswift <45301528+kalebswift@users.noreply.github.com>
2020-01-26 15:23:52 -0500

    finished creating null model submission

--- project/src/models/train_model.R
+++ project/src/models/train_model.R
@@ -8,7 +8,7 @@ library(Metrics)
 DT<-fread('./project/volume/models/data/raw/Stat_380_train.csv')
 train<-DT[!is.na(DT$SalePrice)]
 DT2<-fread('./project/volume/models/data/raw/Stat_380_test.csv')
-test<-DT2[!is.na(DT2$SalePrice)]
+test<-DT2
 
 # make a null model
 
@@ -18,22 +18,22 @@ test$Null_model<-avg_price
 
 
 # get root mean squared error
-rmse(test$SalePrice,test$Null_model)
+rmse(train$SalePrice,test$Null_model)
 
 
 #group by BldgType first to make a little more interesting model
 
-type_price<-train[,.(ap_avg_price=mean(SalePrice)),by=BldgType]
+#type_price<-train[,.(ap_avg_price=mean(SalePrice)),by=BldgType]
 
-setkey(type_price,BldgType)
-setkey(test,BldgType)
+#setkey(type_price,BldgType)
+#setkey(test,BldgType)
 
-test<-merge(test,type_price, all.x=T)
+#test<-merge(test,type_price, all.x=T)
 
 
 # get rmse
-rmse(test$DepDelay,test$ap_avg_price)
+#rmse(train$SalePrice,test$ap_avg_price)
 
 
 # make a submit file
-fwrite(test[,.(ap_avg_price)],"./project/volume/models/data/processed/submit.csv")
+fwrite(test[,.(Id, Null_model)],"./project/volume/models/data/processed/submit.csv")
