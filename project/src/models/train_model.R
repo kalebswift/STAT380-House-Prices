4931260e5722f79a4df17a73bdc3b2d936f54057
kalebswift <45301528+kalebswift@users.noreply.github.com>
2020-02-04 20:39:35 -0500

    fixed folder structore

--- project/src/models/train_model.R
+++ project/src/models/train_model.R
@@ -7,38 +7,16 @@ library(caret)
 
 # Create the test dataframe
 
-DT<-fread('./project/volume/models/data/raw/Stat_380_train.csv')
+DT<-fread('./project/volume/data/raw/Stat_380_train.csv')
 train<-DT[!is.na(DT$SalePrice)]
-DT2<-fread('./project/volume/models/data/raw/Stat_380_test.csv')
+DT2<-fread('./project/volume/data/raw/Stat_380_test.csv')
 test<-DT2
-
-
-# create submission dataframe
-#submit <- test[,.(Id, Null_model)]
-#names(submit)[names(submit) == "Null_model"] = "SalePrice"
-
 
 # Begin working on lm model
 
 set.seed(77)
 
-train_y <- train$SalePrice
 test$SalePrice <- 0
-
-
-#dummies <- dummyVars(SalePrice ~ ., data=train)
-dummies <- dummyVars(SalePrice ~ LotFrontage + LotArea + BldgType + OverallQual + OverallCond + FullBath + HalfBath + TotRmsAbvGrd + YearBuilt + TotalBsmtSF + BedroomAbvGr + Heating + CentralAir + GrLivArea + PoolArea + YrSold, data=train)
-
-
-#train <- predict(dummies, newdata = train)
-#test <- predict(dummies, newdata = test)
-
-# reformat and add back response
-
-#train <- data.table(train)
-#train$SalePrice <- train_y
-#test <- data.table(test)
-
 
 # fit a linear model
 
@@ -50,8 +28,7 @@ summary(lm_model)
 
 # save the model
 
-#saveRDS(dummies,"./project/volume/models/SalePrice_lm.dummies")
-saveRDS(lm_model,"./project/volume/models/SalePrice_lm.model")
+saveRDS(lm_model,"./project/volume/SalePrice_lm.model")
 
 test$pred<-predict(lm_model,newdata = test)
 
@@ -61,4 +38,4 @@ test$pred<-predict(lm_model,newdata = test)
 submit <- test[,.(Id, pred)]
 names(submit)[names(submit) == "pred"] = "SalePrice"
 
-fwrite(submit,"./project/volume/models/data/processed/submit.csv")
+fwrite(submit,"./project/volume/data/processed/submit.csv")
